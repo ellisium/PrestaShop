bd2b75f75dda8ad1c5951e48a16178c689943973
Eric Le Lay <eric.lelay@gmail.com>
2013-09-28 10:08:21 +0200

    Classname issue HHVM :: rename Collection to Collection_hhvm

--- classes/shop/ShopGroup.php
+++ classes/shop/ShopGroup.php
@@ -66,7 +66,7 @@ class ShopGroupCore extends ObjectModel
 
 	public static function getShopGroups($active = true)
 	{
-		$groups = new Collection('ShopGroup');
+		$groups = new Collection_hhvm('ShopGroup');
 		$groups->where('deleted', '=', false);
 		if ($active)
 			$groups->where('active', '=', true);
