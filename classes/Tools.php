3c71d2a6fb72535782d8e269ff2a3fa6cb4011fc
Eric Le Lay <eric.lelay@gmail.com>
2013-10-23 18:20:47 +0200

    enable rewrite url for akumajs

--- classes/Tools.php
+++ classes/Tools.php
@@ -2434,10 +2434,10 @@ exit;
 	}
 
 	public static function modRewriteActive()
-	{
+	{	if($_SERVER['SERVER_SOFTWARE']=='NODEJS'){$ak=true;}
 		if (Tools::apacheModExists('mod_rewrite'))
 			return true;
-		if ($_SERVER['SERVER_SOFTWARE']=='NODEJS'||(isset($_SERVER['HTTP_MOD_REWRITE']) && strtolower($_SERVER['HTTP_MOD_REWRITE']) == 'on') || strtolower(getenv('HTTP_MOD_REWRITE')) == 'on')
+		if ($ak||(isset($_SERVER['HTTP_MOD_REWRITE']) && strtolower($_SERVER['HTTP_MOD_REWRITE']) == 'on') || strtolower(getenv('HTTP_MOD_REWRITE')) == 'on'){if($ak){$_ENV['modRewriteActive']=true;} return true;}
 				return true;
 		return false;
 	}
